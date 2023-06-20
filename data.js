253b11f0df3acb412bc0ebc5da524ae7ccca2c2e
RadaRish <rada.rish@ya.ru>
2023-06-20 18:36:39 +0300

    тур с названиями

---
+++ data.js
@@ -0,0 +1,319 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "терраса",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.04093590427404692,
+        "pitch": 0.009076168933912498,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8577918602469357,
+          "pitch": 0.10735302403643843,
+          "rotation": 0,
+          "target": "1---"
+        },
+        {
+          "yaw": -0.8625227442112386,
+          "pitch": 0.09075311208855119,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": -2.3824416648707896,
+          "pitch": 0.058580439182932764,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1---",
+      "name": "кухня на террасе",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.9054972056836554,
+        "pitch": -0.03760127129769586,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9189617964624297,
+          "pitch": 0.05962287410134515,
+          "rotation": 0,
+          "target": "2-----"
+        },
+        {
+          "yaw": 2.530852118556613,
+          "pitch": 0.07669447986031486,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-----",
+      "name": "лежаки и место для загара",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.125508574123648,
+        "pitch": -0.048656484256206056,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.887751945622348,
+          "pitch": 0.11991554389316406,
+          "rotation": 0,
+          "target": "1---"
+        },
+        {
+          "yaw": -2.5077502398030163,
+          "pitch": 0.1177946592787027,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "мангальная зона",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.2751273566325576,
+        "pitch": 0.04538084466961223,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6053014593725692,
+          "pitch": 0.1043057341908753,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.2458101680953408,
+          "pitch": 0.05417224207295668,
+          "rotation": 0,
+          "target": "2-----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "апартаменты",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.026655937666816953,
+        "pitch": 0.01685574230585729,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3571731337088515,
+          "pitch": 0.09413493400575312,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.92265546701538,
+          "pitch": 0.1119130691664445,
+          "rotation": 0,
+          "target": "5---"
+        },
+        {
+          "yaw": -2.743214146761648,
+          "pitch": 0.11816055265233771,
+          "rotation": 0,
+          "target": "6--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5---",
+      "name": "кухня в апартаментах",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.07425597117837057,
+        "pitch": -0.0025931911239815975,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20105602712183668,
+          "pitch": 0.17061235073817116,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--",
+      "name": "санузел-душ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.027607935440647324,
+        "pitch": -0.009075738396393973,
+        "fov": 1.4301449048735997
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08822304698740702,
+          "pitch": 0.18399305978922698,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
